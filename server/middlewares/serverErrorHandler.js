9251ee6fc93c948fd0d2fd09aa6e044d2fd2958d
ayodejiAA <afolabiayodejia@gmail.com>
2020-02-14 06:56:28 +0100

    set up heroku

--- server/middlewares/serverErrorHandler.js
+++ server/middlewares/serverErrorHandler.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 // eslint-disable-next-line no-unused-vars
 export default (err, _req, res, next) => {
+  console.error(err.stack);
   res.status(500).json({
     status: 'error',
     error: 'internal server error'
